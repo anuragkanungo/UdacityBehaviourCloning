fcf4962a05b13457c8f1ef6fdceb769f5816b38b
Anurag Kanungo <akanungo@uber.com>
2017-11-04 16:08:51 -0400

    update code

--- model.py
+++ model.py
@@ -1,69 +1,33 @@
-import os
-import csv
-from keras import backend as K
-from keras.models import Sequential, model_from_json
-from keras.layers.core import Dense, Activation, Flatten, Dropout, Lambda
+from keras.models import Sequential
+from keras.layers.core import Dense, Flatten, Dropout, Lambda
 from keras.layers.convolutional import Convolution2D, Cropping2D
-from keras.layers.pooling import MaxPooling2D
-from keras.layers.advanced_activations import ELU
-from keras.regularizers import l2, activity_l2
 from keras.optimizers import Adam
-from keras.callbacks import ModelCheckpoint, Callback
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
-import math
-import numpy as np
-from PIL import Image         
-import cv2                 
-import matplotlib.pyplot as plt
-from os import getcwd
+import os
 import csv
-# Fix error with TF and Keras
-import tensorflow as tf
-tf.python.control_flow_ops = tf
-from keras.models import load_model
-
-samples = []
-with open('/Users/traxex/Desktop/driving_log.csv') as csvfile:
-    reader = csv.reader(csvfile)
-    for line in reader:
-        samples.append(line)
-
-with open('/Users/traxex/Desktop/7/driving_log.csv') as csvfile:
-    reader = csv.reader(csvfile)
-    for line in reader:
-        samples.append(line)
-
-# with open('/Users/traxex/Desktop/4/driving_log.csv') as csvfile:
-#     reader = csv.reader(csvfile)
-#     for line in reader:
-#         samples.append(line)
-
-# with open('/Users/traxex/Desktop/5/driving_log.csv') as csvfile:
-#     reader = csv.reader(csvfile)
-#     for line in reader:
-#         samples.append(line)
-
-# with open('/Users/traxex/Downloads/data/driving_log.csv') as csvfile:
-#     reader = csv.reader(csvfile)
-#     for line in reader:
-#         samples.append(line)
-
-
-from sklearn.model_selection import train_test_split
-train_samples, validation_samples = train_test_split(samples, test_size=0.2)
-
-import cv2
 import numpy as np
-import sklearn
+import cv2       
+import tensorflow as tf
+# Fix error with TF and Keras
+tf.python.control_flow_ops = tf
 
+
+# Get data from file and create train validation splits
+def get_train_validation_samples(file_path):   
+    samples = []
+    with open(file_path) as csvfile:
+        reader = csv.reader(csvfile)
+        for line in reader:
+            samples.append(line)
+
+    return train_test_split(samples, test_size=0.2)
+
+# Generator to batch generate train/validation samples
 def generator(samples, batch_size=32):
-    num_samples = len(samples)
-    while 1: # Loop forever so the generator never terminates
-        shuffle(samples)
-        for offset in range(0, num_samples, batch_size):
+    while True:
+        for offset in range(0, len(samples), batch_size):
             batch_samples = samples[offset:offset+batch_size]
-
             images = []
             angles = []
             for batch_sample in batch_samples:
@@ -73,18 +37,20 @@ def generator(samples, batch_size=32):
                 images.append(center_image)
                 angles.append(center_angle)
 
-            # trim image to only see section with road
             X_train = np.array(images)
             y_train = np.array(angles)
-            X_train, y_train = sklearn.utils.shuffle(X_train, y_train)
+            
+            # Shuffle the data
+            X_train, y_train = shuffle(X_train, y_train)
             yield (X_train, y_train)
 
 
-# compile and train the model using the generator function
+# Generate Data
+train_samples, validation_samples = get_train_validation_samples("./data/driving_log.csv")
 train_generator = generator(train_samples, batch_size=32)
 validation_generator = generator(validation_samples, batch_size=32)
 
-
+# Model
 model = Sequential()
 model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
 model.add(Cropping2D(cropping=((50,20), (0,0))))
@@ -100,6 +66,5 @@ model.add(Dense(50))
 model.add(Dense(10))
 model.add(Dense(1))
 model.compile(loss='mse', optimizer='adam')
-model = load_model('model_orig3.h5')
 model.fit_generator(train_generator, samples_per_epoch= len(train_samples), validation_data=validation_generator, nb_val_samples=len(validation_samples), nb_epoch=5)
-model.save('model_orig4.h5')
+model.save('model.h5')
